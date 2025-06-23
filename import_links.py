7eee10fdc1b4d456502a09e8acd2f9e68db2d77b
bjsilva1 <bryanjsilva04@gmail.com>
2025-06-22 19:39:25 -0500

    Changed ALL_LINKS to a dict

--- import_links.py
+++ import_links.py
@@ -1,10 +1,10 @@
 from collections import namedtuple
 
-LinkInfo = namedtuple('LinkInfo', ['name', 'alexander_nullity', 'jones_nullity', 'seifert_nullity', 'signature', 'linking_matrix_zero'])
+LinkInfo = namedtuple('LinkInfo', ['num_components', 'alexander_nullity', 'jones_nullity', 'seifert_nullity', 'signature', 'linking_matrix_zero'])
 
-ALL_LINKS = []
+ALL_LINKS = dict()
 
-with open("KnotTheory_TAMUREU/links_info.txt") as infile:
+with open("KnotTheory_TAMUREU/link_info.txt") as infile:
     for line in infile.readlines():
         link_info = eval(line)
-        ALL_LINKS.append(LinkInfo(link_info[0], link_info[1], link_info[2], link_info[3], link_info[4], link_info[5]))
+        ALL_LINKS[link_info[0]] = LinkInfo(link_info[1], link_info[2], link_info[3], link_info[4], link_info[5], link_info[6])
